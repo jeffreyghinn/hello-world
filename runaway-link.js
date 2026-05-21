b870b94f12621e6e0ab3edfb85106230d61f9a07
jeffreyghinn <jeffreyghinn@outlook.com>
2026-05-21 18:18:01 -0400

    Add mouse movement event listener to runaway-link.js

--- runaway-link.js
+++ runaway-link.js
@@ -1,2 +1,5 @@
 /* Grab the link element from the page and store it in a variable for easy access */
 const link = document.getElementById("runaway-terms-link");
+
+/* Listen for mouse movement anywhere on the page. Every time the mouse moves this function runs again. */
+document.addEventListener()
