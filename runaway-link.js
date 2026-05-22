5e5a508a49517cfe372cae4a15c1b037c653fb56
jeffreyghinn <jeffreyghinn@outlook.com>
2026-05-22 08:45:46 -0400

    Refactor comments in runaway-link.js
    Updated comments for clarity and readability.

--- runaway-link.js
+++ runaway-link.js
@@ -1,7 +1,10 @@
 /* Grab the link element from the page and store it in a variable for easy access */
 const link = document.getElementById("runaway-terms-link");
 
-/* Listen for mouse movement anywhere on the page. Every time the mouse moves this function runs again. */
+/*
+Listen for mouse movement anywhere on the page.
+Every time the mouse moves this function runs again.
+*/
 document.addEventListener(
   "mousemove", (mouseMove) => {
     /* Get the link's size and location */
